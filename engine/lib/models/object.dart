a6282aadd5f69e0dd52d0319c8d46e69c85a402a
TejasKumar3005 <tejaskumar3005@gmail.com>
2024-01-23 21:43:06 +0530

    obj

--- engine/lib/models/object.dart
+++ engine/lib/models/object.dart
@@ -31,7 +31,6 @@ class Object {
 }
 
 
-<<<<<<< HEAD
 // class Character extends Object {
 
 //   //  character has an image
@@ -49,18 +48,3 @@ class Object {
 
   
 // }
-=======
-class Character extends Object {
-  // Character has a name
-  String name;
-  // Character has an image 
-  
-  // Constructor with named parameters
-  Character({required this.name, required double x, required double y, required double width, required double height}) : super(x: x, y: y, width: width, height: height);
-
-  @override
-  String toString() {  
-    return 'Character{name: $name, x: $x, y: $y, width: $width, height: $height}';
-  }
-}
->>>>>>> 56688e7226c3b9c7800e6a5ac2e93a96115aa4ce
