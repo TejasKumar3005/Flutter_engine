c230cd78186caec1529c538019125738a130ea7c
TejasKumar3005 <tejaskumar3005@gmail.com>
2024-01-24 18:39:42 +0530

    game data

--- engine/lib/models/game_data_model.dart
+++ engine/lib/models/game_data_model.dart
@@ -1,9 +1,35 @@
 // Build Game data class to store all relevant game data
 
 import 'package:engine/utils/Image.dart';
-
+class Variable {
+  String name;
+  String type;
+  //value can be a string, int, or double, bool or list
+  dynamic value;
+  Variable(this.name, this.type, this.value);
+}
 class GameData {
   VersatileImage? backgroundImage;
   Map<String, VersatileImage> character_images = {};
+  Map<String, Variable> variables = {};
+
+  GameData(
+      {this.backgroundImage,
+      this.character_images = const {},
+      this.variables = const {}});
+
+
+  factory GameData.fromJson(Map<String, dynamic> json) {
+    GameData gameData = GameData();
+    gameData.backgroundImage = VersatileImage.assetPng(json['background_image']);
+    for (var character in json['characters']) {
+      gameData.character_images[character['name']] = VersatileImage.assetPng(character['image']);
+    }
+    for (var variable in json['variables']) {
+      gameData.variables[variable['name']] = Variable(variable['name'], variable['type'], variable['value']);
+    }
+    return gameData;
+  }
+
   
 }
