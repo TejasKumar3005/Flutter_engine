d9bc36e660a3cf843245ba3726b633303f0cc58c
TejasKumar3005 <tejaskumar3005@gmail.com>
2024-02-23 02:46:32 +0530

    final json

--- engine/lib/models/object.dart
+++ engine/lib/models/object.dart
@@ -8,7 +8,6 @@ import 'package:flutter/material.dart';
 import 'dart:async';
 
 import 'package:flame/components.dart';
-
 class Object extends SpriteComponent
     with HasGameReference<MyGame>, CollisionCallbacks, DragCallbacks {
   Object({name, position, size, image ,required this.isStatic})
@@ -26,7 +25,31 @@ class Object extends SpriteComponent
 
   @override
   void update(double dt) {
+    // Your update logic here
+  }
 
+  // Override DragCallbacks methods
+  @override
+  bool onDragStart(int pointerId, DragStartInfo info) {
+    // Only start dragging if isStatic is false
+    return !isStatic;
+  }
+
+  @override
+  bool onDragUpdate(int pointerId, DragUpdateInfo info) {
+    // Only allow dragging if isStatic is false
+    if (!isStatic) {
+      position.add(info.delta.game);
+      return true;
+    }
+    return false;
+  }
+
+  @override
+  bool onDragEnd(int pointerId, DragEndInfo info) {
+    // You can implement any logic needed when drag ends
+    // For example, snapping the object back into a grid or boundaries
+    return !isStatic;
   }
 
   Future<Image> getImage(String path) async {
