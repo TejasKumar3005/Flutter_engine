765ad1fecfb10f4ed506a228ac0ce0d8e5db1e52
adityaraj-09 <itsrishabhforyou@gmail.com>
2024-03-16 17:45:55 +0530

    added puzzle

--- engine/lib/utils/gameWidgets/puzzlegame.dart
+++ engine/lib/utils/gameWidgets/puzzlegame.dart
@@ -1,4 +1,14 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+import 'dart:math';
+import 'dart:typed_data';
+import 'dart:ui' as ui;
+import 'package:engine/utils/puzzlefunctions.dart';
 import 'package:flutter/material.dart';
+import 'package:image/image.dart' as img;
+import 'package:http/http.dart' as http;
+import 'package:provider/provider.dart';
 
 class PuzzleGame extends StatefulWidget {
   final List<String> imageUrls;
@@ -12,75 +22,245 @@ class PuzzleGame extends StatefulWidget {
 class _PuzzleGameState extends State<PuzzleGame> {
   List<String> shuffledImageUrls = [];
   int? currentIndex;
+  List<String> urls = [];
+  late Timer _timer;
+  int _seconds = 0;
+  List<img.Image> puzzleTiles = [];
+  ImageSlicer imageSlicer = ImageSlicer();
+  String gameStatus = "NS";
+  Map<int, int> occupied = {};
+  void fetchImage() async {
+    var res = await http.get(Uri.parse("/url"));
+    if (res.statusCode == 200) {
+      setState(() {
+        urls = jsonDecode(res.body);
+        shuffledImageUrls = List.from(urls)..shuffle();
+      });
+    }
+  }
+
+  void startTimer() {
+    const oneSec = Duration(seconds: 1);
+    _timer = Timer.periodic(oneSec, (timer) {
+      Provider.of<ImageSlicer>(context, listen: false).changetime();
+    });
+  }
+
+  late Map<int, Offset> imagePositions;
+  List<Offset> randomPos = [];
+  late List<Offset> offsetsOfDragTargets;
+  List<int> overlappingTargets = [];
 
   @override
   void initState() {
     super.initState();
+
+    img.Image originalImage =
+        img.decodeImage(File("assets/bg.jpeg").readAsBytesSync())!;
+    puzzleTiles = imageSlicer.sliceImage(originalImage, 3, 3)..shuffle();
     shuffledImageUrls = List.from(widget.imageUrls)..shuffle();
+    imagePositions = {};
+    WidgetsBinding.instance?.addPostFrameCallback((_) {
+      // Initialize randomPos here after the first frame has been drawn
+      randomPos = List.generate(widget.imageUrls.length, (index) {
+        final screenWidth = MediaQuery.of(context).size.width;
+        final screenHeight = MediaQuery.of(context).size.height;
+        final randomX = Random().nextInt(screenWidth.toInt() - 100).toDouble();
+        final randomY = Random().nextInt(screenHeight.toInt() - 100).toDouble();
+        return Offset(randomX, randomY);
+      });
+      // Call setState to rebuild the widget with the updated randomPos
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Puzzle Game'),
-      ),
-      body: Center(
-        child: SizedBox(
-          height: 150,
-          width: 150,
-          child: GridView.builder(
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 3, 
-              childAspectRatio: 1.0,// Adjust as per your puzzle size
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        double viewportWidth = constraints.maxWidth;
+     
+      
+        print("Viewport width: $viewportWidth");
+        return Scaffold(
+          backgroundColor: Colors.black,
+          body: Stack(children: [
+            Center(
+              child: Container(
+                height: 300,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage("assets/bg.jpeg"),
+                        fit: BoxFit.fill,
+                        filterQuality: FilterQuality.low),
+                    border: Border.all(color: Colors.white, width: 2)),
+                width: 300,
+              ),
             ),
-            itemCount: widget.imageUrls.length,
-            itemBuilder: (context, index) {
-              return DragTarget<int>(
-                onAccept: (int data) {
+            Center(
+              child: Container(
+                height: 300,
+                decoration: BoxDecoration(
+                    color: Colors.yellow.withOpacity(0.5),
+                    border: Border.all(color: Colors.white, width: 2)),
+                width: 300,
+                child: GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    childAspectRatio: 1.0, // Adjust as per your puzzle size
+                  ),
+                  itemCount: widget.imageUrls.length,
+                  itemBuilder: (context, index) {
+                    return DragTarget<int>(
+                      key: Key(index.toString()),
+                      onWillAccept: (data) {
+                        print("-----will accept");
+                        return true;
+                      },
+                      onMove: (details) {
+                        print("-----moving");
+                      },
+                      onAccept: (int data) {
+                        setState(() {
+                          imagePositions[data] = Offset(
+                            puzzleOffsets(context)[0] + index % 3 * 100.0,
+                            puzzleOffsets(context)[1] + index ~/ 3 * 100.0,
+                          );
+                        });
+                      },
+                      builder: (context, candidateData, rejectedData) {
+                        return Container(
+                          width: 100,
+                          height: 100,
+                          decoration: BoxDecoration(
+                              border:
+                                  Border.all(color: Colors.black, width: 0.3)),
+                          child: Container(),
+                        );
+                      },
+                    );
+                  },
+                ),
+              ),
+            ),
+            ..._buildScatteredImages(),
+            Visibility(
+              visible: gameStatus == "NS" || gameStatus == "PS",
+              child: Positioned(
+                  top: (MediaQuery.of(context).size.height - 100) / 2,
+                  left: (MediaQuery.of(context).size.width - 100) / 2,
+                  child: Transform.rotate(
+                    angle: 0.4,
+                    child: GestureDetector(
+                      onTap: () {
+                        startTimer();
+                        setState(() {
+                          gameStatus = "ST";
+                        });
+                      },
+                      child: Container(
+                        height: 100,
+                        width: 100,
+                        decoration: BoxDecoration(
+                            image: DecorationImage(
+                                image:
+                                    AssetImage("assets/icons/Play (3).png"))),
+                      ),
+                    ),
+                  )),
+            ),
+            Visibility(
+                visible: gameStatus == "ST" || gameStatus == "PS",
+                child: TimerWidget()),
+          ]),
+        );
+      },
+    );
+  }
+
+  List<double> puzzleOffsets(BuildContext context) {
+    List<double> off = [];
+    double offX = (MediaQuery.of(context).size.width - 300) / 2;
+    double offY = (MediaQuery.of(context).size.height - 300) / 2;
+    off.add(offX);
+    off.add(offY);
+    return off;
+  }
+
+  List<int> getCurrentOverlappingTargets(Offset localOffset,
+      List<Offset> targetOffsets, double targetWidth, double targetHeight) {
+    List<int> overlappingTargets = [];
+    for (int i = 0; i < targetOffsets.length; i++) {
+      Offset targetOffset = targetOffsets[i];
+      // Check if the local offset of the dragged item lies within the bounds of the target
+      if (localOffset.dx >= targetOffset.dx &&
+          localOffset.dx <= targetOffset.dx + targetWidth &&
+          localOffset.dy >= targetOffset.dy &&
+          localOffset.dy <= targetOffset.dy + targetHeight) {
+        print("____index-> " + i.toString());
+        overlappingTargets.add(i); // Add the index of the target to the list
+      }
+    }
+    return overlappingTargets;
+  }
+
+  List<Widget> _buildScatteredImages() {
+    return gameStatus == "NS"
+        ? [Container()]
+        : List.generate(puzzleTiles.length, (index) {
+            final position = imagePositions[index] ?? randomPos[index];
+            return Positioned(
+              key: Key(index.toString()),
+              left: position.dx,
+              top: position.dy,
+              child: Draggable<int>(
+                key: Key(index.toString()),
+                data: index,
+                child: Container(
+                  width: 100,
+                  height: 100,
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black, width: 0.3)),
+                  child: Image.memory(
+                    Uint8List.fromList(img.encodePng(puzzleTiles[index])),
+                    fit: BoxFit.fill,
+                  ),
+                ),
+                feedback: Container(
+                  width: 100,
+                  height: 100,
+                  child: Image.memory(
+                    Uint8List.fromList(img.encodePng(puzzleTiles[index])),
+                    fit: BoxFit.fill,
+                  ),
+                ),
+                childWhenDragging: Container(),
+                onDragStarted: () {
+                  print("---darg started");
                   setState(() {
-                    final temp = shuffledImageUrls[index];
-                    shuffledImageUrls[index] = shuffledImageUrls[data];
-                    shuffledImageUrls[data] = temp;
+                    currentIndex = index;
+                  });
+                },
+                onDragCompleted: () {
+                  setState(() {
                     currentIndex = null;
                   });
                 },
-                builder: (context, candidateData, rejectedData) {
-                  return Draggable<int>(
-                    feedback: Container(
-                      width: 50,
-                      height: 50,
-                      child: Image.asset("assets/icons/"+
-                        shuffledImageUrls[index],
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                    child:  Container(
-                      width: 50,
-                      height: 50,
-                      child:currentIndex != index
-                        ? Image.asset("assets/icons/"+
-                        shuffledImageUrls[index],
-                        fit: BoxFit.cover,
-                      ):Container(),
-                    )
-                        , // Hide the original image when dragging
-                    data: index, // Pass index as data when dragging
-                    
-                    onDragStarted: () {
-                      setState(() {
-                        currentIndex = index;
-                      });
-                    },
-                  );
+                onDraggableCanceled: (Velocity velocity, Offset offset) {
+                  // Reset the position if the drag operation is canceled
+                  setState(() {
+                    imagePositions[index] = offset;
+                    currentIndex = null;
+                  });
                 },
-              );
-            },
-          ),
-        ),
-      ),
-    );
+              ),
+            );
+          });
   }
 }
-
-
