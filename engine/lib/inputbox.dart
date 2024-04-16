b8b9b780146cf83afa0e1650d17d1e49a5cb7815
adityaraj-09 <itsrishabhforyou@gmail.com>
2024-04-16 19:04:25 +0530

    add

--- engine/lib/inputbox.dart
+++ engine/lib/inputbox.dart
@@ -43,20 +43,43 @@ class _KafkaMessageWidgetState extends State<KafkaMessageWidget> {
   StateMachineController? stateMachineController;
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
-  Future<Map<String, dynamic>> getJson() async {
-    http.Response response = await http.post(
-        Uri.parse("http://35.165.158.80:9092/send_data"),
-        body: jsonEncode(
-          {"msg": _emailController.text.toString()},
+  Future<Map<String, dynamic>> getJson(BuildContext context) async {
+    try {
+      print("---msg    "+_emailController.text.toString());
+      http.Response response = await http.post(
+          Uri.parse("http://35.165.158.80:9092/send_data"),
+          body: jsonEncode(
+            {"msg": _emailController.text.toString()},
+          ),
+          headers: <String, String>{
+            'content-Type': 'application/json; charset=UTF-8'
+          });
+      if (response.statusCode != 200) {
+        Navigator.of(context).pop();
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text(
+            jsonDecode(response.body)["message"],
+          ),
+          backgroundColor: Colors.red,
+        ));
+        failTrigger?.fire();
+        return {};
+      }
+      successTrigger?.fire();
+      return jsonDecode(response.body)["message"];
+    } catch (e) {
+      Navigator.of(context).pop();
+      print(e.toString());
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        action: SnackBarAction(label: "ok", onPressed: () {}),
+        content: Text(
+          e.toString(),
         ),
-        headers: <String, String>{
-          'content-Type': 'application/json; charset=UTF-8'
-        });
-    if (response.statusCode != 200) {
+        backgroundColor: Colors.red,
+      ));
+      failTrigger?.fire();
       return {};
     }
-
-    return jsonDecode(response.body)["message"];
   }
 
   @override
@@ -71,13 +94,7 @@ class _KafkaMessageWidgetState extends State<KafkaMessageWidget> {
         stateMachineController =
             StateMachineController.fromArtboard(artboard, "Login Machine");
         if (stateMachineController != null) {
-      
           artboard.addController(stateMachineController!);
-
-          stateMachineController!.inputs.forEach((e) {
-            debugPrint(e.runtimeType.toString());
-            debugPrint("name${e.name}End");
-          });
 
           stateMachineController!.inputs.forEach((element) {
             if (element.name == "trigSuccess") {
@@ -93,7 +110,6 @@ class _KafkaMessageWidgetState extends State<KafkaMessageWidget> {
             }
           });
         }
-        
 
         setState(() => _teddyArtboard = artboard);
       },
@@ -101,9 +117,7 @@ class _KafkaMessageWidgetState extends State<KafkaMessageWidget> {
   }
 
   void handsOnTheEyes() {
-    
     isHandsUp?.change(true);
-  
   }
 
   void lookOnTheTextField() {
@@ -131,120 +145,164 @@ class _KafkaMessageWidgetState extends State<KafkaMessageWidget> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xffd6e2ea),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            if (_teddyArtboard != null)
-              SizedBox(
-                width: 400,
-                height: 300,
-                child: Rive(
-                  artboard: _teddyArtboard!.instance(),
-                  fit: BoxFit.fitHeight,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              if (_teddyArtboard != null)
+                SizedBox(
+                  width: 400,
+                  height: 300,
+                  child: Rive(
+                    artboard: _teddyArtboard!,
+                    fit: BoxFit.fitHeight,
+                  ),
                 ),
-              ),
-            Container(
-              alignment: Alignment.center,
-              width: 400,
-              padding: const EdgeInsets.only(bottom: 15),
-              margin: const EdgeInsets.only(bottom: 15 * 4),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 15),
-                    child: Column(
-                      children: [
-                        const SizedBox(height: 15 * 2),
-                        TextField(
-                          controller: _emailController,
-                          onTap: handsOnTheEyes,
-                          onChanged: moveEyeBalls,
-                          keyboardType: TextInputType.emailAddress,
-                          style: const TextStyle(fontSize: 14),
-                          cursorColor: const Color(0xffb04863),
-                          decoration: const InputDecoration(
-                            hintText: "make game on Holi",
-                            filled: true,
-                            border: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(10)),
-                            ),
-                            focusColor: Color(0xffb04863),
-                            focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                color: Color(0xffb04863),
+              Container(
+                alignment: Alignment.center,
+                width: 400,
+                padding: const EdgeInsets.only(bottom: 15),
+                margin: const EdgeInsets.only(bottom: 15 * 4),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 15),
+                      child: Column(
+                        children: [
+                          const SizedBox(height: 15 * 2),
+                          TextField(
+                            controller: _emailController,
+                            onTap: () {
+                              lookOnTheTextField();
+                            },
+                            onChanged: moveEyeBalls,
+                            keyboardType: TextInputType.emailAddress,
+                            style: const TextStyle(fontSize: 14),
+                            cursorColor: const Color(0xffb04863),
+                            decoration: const InputDecoration(
+                              hintText: "make game on Holi",
+                              filled: true,
+                              border: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(10)),
                               ),
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(10)),
+                              focusColor: Color(0xffb04863),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                  color: Color(0xffb04863),
+                                ),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(10)),
+                              ),
                             ),
                           ),
-                        ),
-                        const SizedBox(height: 15),
-                        const SizedBox(height: 15),
-                        Row(
-                          crossAxisAlignment: CrossAxisAlignment.end,
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            //remember me checkbox
-                            // Row(
-                            //   children: [
-                            //     Checkbox(
-                            //       value: false,
-                            //       onChanged: (value) {},
-                            //     ),
-                            //     const Text("Remember me"),
-                            //   ],
-                            // ),
-                            ElevatedButton(
-                              onPressed: () {
-                                showDialog(
-                                  context: context,
-                                  barrierDismissible: false,
-                                  builder: (BuildContext context) {
-                                    return RiveAnimationDialog();
-                                  },
-                                );
-                                getJson().then((value) => {
-                                      Navigator.push(
-                                          context,
-                                          MaterialPageRoute(
-                                              builder: (context) =>
-                                                  Game(gameJson: value)))
-                                    });
-                              },
-                              style: ElevatedButton.styleFrom(
-                                backgroundColor: const Color(0xffb04863),
+                          const SizedBox(height: 15),
+                          TextField(
+                            controller: _passwordController,
+                            onTap: handsOnTheEyes,
+                            keyboardType: TextInputType.visiblePassword,
+                            style: const TextStyle(fontSize: 14),
+                            cursorColor: const Color(0xffb04863),
+                            decoration: const InputDecoration(
+                              hintText: "API key",
+                              filled: true,
+                              border: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(10)),
                               ),
-                              child: const Row(
-                                children: [
-                                  Text(
-                                    "Generate Game",
-                                    style: TextStyle(color: Colors.white),
-                                  ),
-                                  SizedBox(
-                                    width: 20,
-                                  ),
-                                  Icon(
-                                    Icons.arrow_circle_right,
-                                    color: Colors.white,
-                                  )
-                                ],
+                              focusColor: Color(0xffb04863),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                  color: Color(0xffb04863),
+                                ),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(10)),
                               ),
                             ),
-                          ],
-                        ),
-                      ],
+                          ),
+                          SizedBox(
+                            height: 15,
+                          ),
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              //remember me checkbox
+                              // Row(
+                              //   children: [
+                              //     Checkbox(
+                              //       value: false,
+                              //       onChanged: (value) {},
+                              //     ),
+                              //     const Text("Remember me"),
+                              //   ],
+                              // ),
+                              ElevatedButton(
+                                onPressed: () {
+                                  if (_emailController.text.isNotEmpty) {
+                                    showDialog(
+                                      context: context,
+                                      barrierDismissible: false,
+                                      builder: (BuildContext context) {
+                                        return RiveAnimationDialog();
+                                      },
+                                    );
+                                    getJson(context).then((value) => {
+                                          print(value.toString()),
+                                          if (value.isNotEmpty)
+                                            {
+                                              Navigator.push(
+                                                  context,
+                                                  MaterialPageRoute(
+                                                      builder: (context) =>
+                                                          Game(
+                                                              gameJson: value)))
+                                            }
+                                        });
+                                  } else {
+                                    ScaffoldMessenger.of(context)
+                                        .showSnackBar(SnackBar(
+                                      content: Text(
+                                        "prompt cannot be empty",
+                                      ),
+                                      backgroundColor: Colors.red,
+                                    ));
+                                  }
+                                },
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: const Color(0xffb04863),
+                                ),
+                                child: const Row(
+                                  children: [
+                                    Text(
+                                      "Generate Game",
+                                      style: TextStyle(color: Colors.white),
+                                    ),
+                                    SizedBox(
+                                      width: 20,
+                                    ),
+                                    Icon(
+                                      Icons.arrow_circle_right,
+                                      color: Colors.white,
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
